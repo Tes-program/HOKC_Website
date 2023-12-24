2b16c0a40adf1cb273d8696ae88daa0610cb5709
Falomo Sharon Simisola <sharyy@Falomos-MacBook-Pro.local>
2023-12-24 14:58:51 +0100

    department page

--- src/components/departmentProps.tsx
+++ src/components/departmentProps.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-// import { departments } from '../constant/department';
+import { departments } from '../constant/department';
 
 export const Department: React.FC = () => {
   return (
-    // <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-10">
-    //   {departments.map((department) => (
-    //     <div
-    //       key={department.id}
-    //       className="flex flex-col items-center justify-center"
-    //     >
-    //       <img
-    //         className="w-20 h-20"
-    //         src={department.image}
-    //         alt={department.alt}
-    //       />
-    //       <p className="text-sm font-Rubik-Regular">{department.department}</p>
-    //     </div>
-    //   ))}
-    // </div>
-
-    <></>
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-10">
+      {departments.map((department) => (
+        <div
+          key={department.id}
+          className="flex flex-col items-center justify-center"
+        >
+          <img
+            className="w-20 h-20"
+            src={department.image}
+            alt={department.alt}
+          />
+          <p className="text-sm font-Rubik-Regular">{department.department}</p>
+        </div>
+      ))}
+    </div>
   );
 };
--- src/pages/Department.tsx
+++ src/pages/Department.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
-const Department: React.FC = () => {
+import { Department } from '../components/departmentProps';
+const DepartmentPage: React.FC = () => {
   return (
-    <div>
-      <h2>Department Page</h2>
-      {/* Add content specific to the Contact page */}
-    </div>
+    <section className="w-full h-full">
+      <div className=" text-white bg-gradient-to-r from-red-500 to-red-950 h-[80vh] flex items-center ">
+        <div className="p-4">
+          <h2 className="text-lg font-semibold pb-6">THE HOKC MINISTERS</h2>
+          <p className="mr-8 pb-4">
+            And he gave some, apostles; and some, prophets; and some,
+            evangelists; and some, pastors and teachers; For the perfecting of
+            the saints, for the work of the ministry, for the edifying of the
+            body of Christ:
+          </p>
+          <p className=" font-light">Ephesians 4:11-12 (KJV)</p>
+        </div>
+        <img src="" alt="" />
+      </div>
+      <Department />
+    </section>
   );
 };
 
-export default Department;
+export default DepartmentPage;
